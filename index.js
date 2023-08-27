1e6a050bcf2ed97a3722f81151a18421c84ccf7f
7u4bgr <abdllyevv@gmail.com>
2023-08-27 14:49:57 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+function MyFunc() {
+    let name = document.getElementById("name").value
+    let surname = document.getElementById("surname").value
+    let age = document.getElementById("age").value
+    console.log(name,surname,age)
+    document.getElementById("h1").innerHTML=name+surname+age
+    
+}
+
+
